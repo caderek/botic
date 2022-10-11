be586d6643b512a76435d199a589b26bcefc6e19
Maciej Caderek <maciej.caderek@gmail.com>
2022-10-11 06:07:29 +0200

    Path movement WIP

--- src/examples/wip.ts
+++ src/examples/wip.ts
@@ -7,13 +7,36 @@ $.listen.key.press.NumpadMultiply.do(async () => {
   await $.mouse.drag.Left.fast.from(860, 350).circular(100, { segments: 8 });
   await $.mouse.drag.Left.fast.from(1090, 350).circular(100, { segments: 5 });
   await $.mouse.drag.Left.fast.from(1320, 350).circular(100, { segments: 3 });
-  await $.mouse.drag.Left.fast.from(600, 700).circular(100);
-  await $.mouse.drag.Left.fast.from(830, 700).circular(100);
-  await $.mouse.drag.Left.fast.from(1060, 700).circular(100);
+
+  await $.mouse.drag.Left.fast.from(400, 700).circular(100);
+  await $.mouse.drag.Left.fast.from(630, 700).circular(100);
+  await $.mouse.drag.Left.fast.from(860, 700).circular(100);
+  await $.mouse.drag.Left.fast.from(515, 800).circular(100);
+  await $.mouse.drag.Left.fast.from(745, 800).circular(100);
+
+  await $.mouse.drag.Left.fast.from(1300, 750).circular(100);
   await $.mouse.drag.Left.fast
-    .from(715, 800)
-    .circular(100, { angle: 270, startAngle: 45 });
+    .from(1400, 650)
+    .circular(100, { startAngle: -90, angle: 270 });
   await $.mouse.drag.Left.fast
-    .from(945, 800)
-    .circular(100, { clockwise: false, angle: 270, startAngle: 45 });
+    .from(1400, 650)
+    .circular(70, { startAngle: -90, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1400, 850)
+    .circular(100, { startAngle: 0, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1400, 850)
+    .circular(70, { startAngle: 0, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1200, 850)
+    .circular(100, { startAngle: 90, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1200, 850)
+    .circular(70, { startAngle: 90, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1200, 650)
+    .circular(100, { startAngle: 180, angle: 270 });
+  await $.mouse.drag.Left.fast
+    .from(1200, 650)
+    .circular(70, { startAngle: 180, angle: 270 });
 });
